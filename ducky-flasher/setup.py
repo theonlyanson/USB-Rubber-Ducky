db63972ccfe7397b8efea1845267afa121d995b0
mike <notmike@users.noreply.github.com>
2018-10-15 01:03:19 -0400

    changed print & raw_input functions to make ducky-flasher compatible on python2/3

--- ducky-flasher/setup.py
+++ ducky-flasher/setup.py
@@ -7,10 +7,10 @@ permission = os.system("sudo chmod 777 ducky-flasher")
 if permission == 0:
     move = os.system("sudo cp ducky-flasher /usr/bin; sudo cp -r Firmware /usr/bin")
     if move == 0:
-        print "ducky-flasher is setup!"
-        print "To use it, type sudo ducky-flasher"
+        print('ducky-flasher is setup!')
+        print('To use it, type sudo ducky-flasher')
     else:
-        print "Make sure you ran setup.py with sudo"
+        print('Make sure you ran setup.py with sudo')
 
 else:
-    print "Make sure you ran setup.py with sudo"
+    print('Make sure you ran setup.py with sudo')
--- ducky-flasher/uninstall.py
+++ ducky-flasher/uninstall.py
@@ -1,20 +1,21 @@
 #Uninstall ducky-flasher
 import os
+from builtins import input
 os.system("clear")
-print "Are you sure you want to uninstall ducky-flahser?"
-print "1 = yes"
-print "2 = no"
-question = int(raw_input("Choice: "))
+print('Are you sure you want to uninstall ducky-flahser?')
+print('1 = yes')
+print('2 = no')
+question = int(input("Choice: "))
 
 if question == 1:
     os.system("clear")
-    print "Uninstalling..."
+    print('Uninstalling...')
     status = os.system("sudo rm /usr/bin/ducky-flasher; rm -r /usr/bin/Firmware")
     if status == 0:
         os.system("clear")
-        print "ducky-flasher has been uninstalled :("
+        print('ducky-flasher has been uninstalled :(')
     else:
-        print "make sure your running as root"
+        print('make sure your running as root')
 else:
     os.system("clear")
-    print "OK, Awesome :P"
+    print('OK, Awesome :P')
